1ded1b68db04559c5c937020467ffb50b4d90162
acch2016 <ie696846@iteso.mx>
2018-04-25 20:00:31 -0500

    2018 04 25 20:00 TODO: send data

--- UDP.py
+++ UDP.py
@@ -7,6 +7,7 @@ import socket
 
 fs, data = wavfile.read('C:/Users/Usuario/Documents/Audacity/Fanfare.wav')
 # fs, data = wavfile.read('C:/Users/Usuario/Downloads/Fanfare_32bits.wav')
+
 print(data)
 print(type(data))
 # print(isinstance(data, tuple))
@@ -17,6 +18,7 @@ print(type(data))
 # print(isinstance(data, enumerate))
 print('%d sys.getsizeof(data)' %sys.getsizeof(data))
 print(data.shape)  # Tuple of array dimensions
+print(data.shape[0])
 print('%s Data-type of the array’s elements' %data.dtype)
 print('%d Number of array dimensions' %data.ndim)  #
 print('%d Number of elements in the array' %data.size)
@@ -26,19 +28,19 @@ print('%d Total bytes consumed by the elements of the array' %data.nbytes)
 # print(data[1, 1])
 # print(data[1, 2]) # No existe columna 2. Va de 0 a 1
 
-# for i in range(0, 441000-1):
-#     for j in range(0, 2):
+# for i in range(0, 285120-1):
+#     for j in range(0, 1):
 #         print(data[i, j])
         # print '%d * %d = %d' % (x, y, x*y)
 
-var1 = str(data[285120-1, 1])
-print(var1.encode('utf-8'))
-
-var = str(data[285120-2, 1])
-print(var.encode('utf-8'))
-
-var2 = str(data)
-print(var2.encode('utf-8'))
+# var1 = str(data[data.shape[0]-1, 1])
+# print(var1.encode('utf-8'))
+#
+# var = str(data[data.shape[0]-2, 1])
+# print(var.encode('utf-8'))
+#
+# var2 = str(data)
+# print(var2.encode('utf-8'))
 
 # var2 = tostring(data)
 # print(var2.encode('utf-8'))
@@ -49,12 +51,14 @@ UDP_PORT = 50005
 
 MESSAGE = "Hello, World!"
 print(MESSAGE.encode('utf-8'))
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
 while True:
     # print(1)
 
-    for i in range(0, 285120-1):
-        for j in range(0, 2):
-            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
-            sock.sendto(data[i, j].encode('utf-8'), (UDP_IP, UDP_PORT))
+    for i in range(0, data.shape[0]-1):
+        for j in range(0, 1):
+
+            sock.sendto(data[i, j], (UDP_IP, UDP_PORT))
     # sock.sendto(MESSAGE.encode('utf-8'), (UDP_IP, UDP_PORT))
     # sock.sendto(var2.encode('utf-8'), (UDP_IP, UDP_PORT))
